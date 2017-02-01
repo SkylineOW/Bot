151f09997036dfa970f06beb5750f90a84c51ac2
Ocky <schalkrabe.sr@gmail.com>
2017-02-01 12:14:34 +0200

    Added periodic updates to bot
    The bot now runs periodic message updates for all guilds that has a
    raffle running. The code added to this file governs the timers and makes
    sure there is only one timer that persists per task.
    
    Revert "Dependancy update"
    
    This reverts commit
    9910dbd7c17c048b2e370f8b9bea83496c0b0189.
    
    Dependancy update
    
    Updated eris and added new relic apm for application
    monitoring.

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,8 @@
 // url to use for adding bots.
+// Production:
 // https://discordapp.com/oauth2/authorize?client_id=272307763898482689&scope=bot&permissions=519239
+// Development
+// https://discordapp.com/oauth2/authorize?client_id=265529528124833802&scope=bot&permissions=519239
 
 // Discord text markup
 // italics = *italics*
@@ -12,15 +15,62 @@
 // underline bold italics = __***underline bold italics***__
 // https://support.discordapp.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-
 
+// embed: {
+//     title: 'This is a title',
+//     description: 'This is a description',
+//     url: 'https://google.com',
+//     color: 6897122,
+//   footer: {
+//     text: 'Footer text',
+//       icon_url: 'https://hydra-media.cursecdn.com/overwatch.gamepedia.com/5/5e/Ability-zenyatta4.png?version=1a445e05537ef0ca20a4d26ce2fecf6b',
+//   },
+//   image: {
+//     url: 'https://hydra-media.cursecdn.com/overwatch.gamepedia.com/9/92/Zenyatta-portrait.png?version=cdf1df79c396c31f1b1001caa2a9180d',
+//       proxy_url: 'https://github.com',
+//       height: 400,
+//   },
+//   thumbnail: {
+//     url: 'https://hydra-media.cursecdn.com/overwatch.gamepedia.com/9/92/Zenyatta-portrait.png?version=cdf1df79c396c31f1b1001caa2a9180d',
+//       proxy_url: 'https://github.com',
+//       height: 400,
+//   },
+//   author: {
+//     name: 'Author',
+//       url: 'https://google.com',
+//       icon_url: 'https://hydra-media.cursecdn.com/overwatch.gamepedia.com/d/d6/Spray_Zenyatta_Cute.png?version=63dd0fed2469f5bab66bc2e465e6fbc1'
+//   },
+//   fields: [
+//     {
+//       name: 'Field name',
+//       value: 'Field value',
+//       inline: true
+//     },
+//     {
+//       name: 'Field name',
+//       value: 'Field value',
+//       inline: true
+//     },
+//     {
+//       name: 'Field name',
+//       value: 'Field value',
+//       inline: true,
+//     }
+//   ],
+// }
+
+const async = require('async');
+const chalk = require('chalk');
 const Eris = require('eris');
 const fs = require('fs');
 const path = require('path');
 const pe = require('utils/error');
 
+const redis = require('data/redis');
+
 const config = require('./config');
 
 //Initialize db connection.
-require('data/mongoose');
+const mongoose = require('data/mongoose');
 
 const bot = new Eris.CommandClient(config.discord.token, {}, {
   description: 'A helpful omnic to help lighten the load of mods.',
@@ -30,7 +80,50 @@ const bot = new Eris.CommandClient(config.discord.token, {}, {
 
 // Event handling for the bot.
 bot.on('ready', () => {
-  console.log(`Ready! Guilds: ${bot.guilds.size}`);
+  // Start an interval for starting monitoring tasks for various guilds.
+  const startMonitoring = async () => {
+    // Only start this if we have storage connections.
+    try {
+
+      if (redis.state === 'connected' && mongoose.state === 'open') {
+        await async.each(bot.guilds.map((guild) => {
+          return guild;
+        }), async (guild) => {
+          // Fetch the status of the guild's raffle and the lock for it.
+          const response = await redis.multi()
+            .get(`Raffle:${guild.id}:state`)
+            .ttl(`Raffle:${guild.id}:lock`)
+            .execAsync();
+
+          const status = response[0];
+          const guildTime = response[1];
+
+          const raffle = require('utils/raffle');
+
+          switch (status) {
+            case raffle.state.started:
+            case raffle.state.inProgress:
+            case raffle.state.closed:
+              if (guildTime <= 0) {
+                // Create a random number as a value to use in the lock.
+                const id = Math.random() * Date.now().valueOf();
+                await raffle.startMonitor(guild.id, id);
+              }
+              break;
+            default:
+              break;
+          }
+        });
+      }
+    }
+    catch(error) {
+      console.log(pe.render(error));
+    }
+
+    setTimeout(startMonitoring, 10000);
+  };
+
+  setTimeout(startMonitoring, 3000);
 });
 
 //Needs to happen before commands are registered since some of them rely on this file.
@@ -65,10 +158,14 @@ const registerCommand = async (parent, dir, file, name) => {
     const data = require(path.join(dir, file));
 
     if (parent === bot) {
-      return await parent.registerCommand(name, data.exec, data.options);
+      const command = await parent.registerCommand(name, data.exec, data.options);
+      console.log(chalk.white(`%s %s from %s has been loaded`), chalk.green('[Command]'), name, path.join(dir, file));
+      return command;
     }
 
-    return await parent.registerSubcommand(name, data.exec, data.options);
+    const command = await parent.registerSubcommand(name, data.exec, data.options);
+    console.log(chalk.white(`%s %s from %s has been loaded`), chalk.green('[Command]'), name, path.join(dir, file));
+    return command;
   }
   catch (error) {
     console.log(`${path.join(dir, file)} could not be loaded:\n` + pe.render(error));
