8a12802d08995fd7c73de432d8617d70201dce0c
Ocky <schalkrabe.sr@gmail.com>
2017-02-01 12:35:08 +0200

    Introduced storage states
    Added a state variable to storage clients to allow checks for
    disconnects. Added clean exits for storage on app termination.

--- src/data/mongoose.js
+++ src/data/mongoose.js
@@ -16,31 +16,43 @@ require('./models/users');
 // ToDo: Create neat notification messages using chalk.
 mongoose.connection.on("connecting", () => {
   console.log("db connecting...");
+  mongoose.state = 'connecting';
 });
 
 mongoose.connection.on("error", (error) => {
   console.log("db error: " + error);
+  mongoose.state = 'disconnected';
 });
 
 mongoose.connection.on("connected", () => {
   console.log("db connected");
+  mongoose.state = 'connected';
 });
 
 mongoose.connection.on("open", () => {
   console.log("db open");
+  mongoose.state = 'open';
 });
 
 mongoose.connection.on("reconnected", () => {
   console.log("db reconnected");
+  mongoose.state = 'connected'
 });
 
 mongoose.connection.on("disconnected", () => {
   console.log("db disconnected");
+  mongoose.state = 'disconnected';
 });
 
 mongoose.AddConnectionEvent = (event, func) => {
   mongoose.connection.on(event, func);
 };
+
+process.on(`SIGINT`, () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose connection closing due to app termination.');
+  })
+})
 
 // Swap out default promise library for something better.
 mongoose.Promise = bluebird;
--- src/data/redis.js
+++ src/data/redis.js
@@ -12,18 +12,32 @@ const client = redis.createClient(config.redis.url);
 // ToDo: Create neat notification messages using chalk.
 client.on('ready', () => {
   console.log(`Redis connected`);
+  client.state = 'connected';
 });
 
 client.on('connect', () => {
   console.log(`Redis connecting...`);
+  client.state = 'connecting';
 });
 
 client.on('reconnect', () => {
   console.log('Redis reconnecting...');
+  client.state = 'reconnecting';
 });
 
 client.on('error', (error) => {
   console.log(`Redis error: ${error}`);
+  client.state = 'error';
+});
+
+client.on('end', () => {
+  console.log(`Redis disconnected.`);
+  client.state = 'disconnected';
+});
+
+process.on('SIGINT', () => {
+  client.quit();
+  console.log('Redis connection closed due to app termination.');
 });
 
 client.AddEvent = (event, func) => {
